87913dab05d4d3779206566be5086265c4c36eae
Vito Hsu <garraypierce@gmail.com>
2025-06-12 14:48:41 +0800

    for cicd

---
+++ test_app.py
@@ -0,0 +1,11 @@
+from app import add, subtract
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(-1, 1) == 0
+    assert add(0, 0) == 0
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(10, 0) == 10
+    assert subtract(7, 7) == 0
